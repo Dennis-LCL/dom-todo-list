eaf7eceaf39e72c7a46db3df2a871d3d25618c9e
Dennis <lichling@gmail.com>
2019-05-25 11:18:16 +0800

    Add Remove Todo Item functionality.

--- script.js
+++ script.js
@@ -1,47 +1,76 @@
 const tasks = ["buy milk", "eat dinner", "nail javascript", "give feedback"];
+const inputPrompt = "What's next?";
 
-let todoList = document.querySelector("ul#todo-list");
-let todoItem;
+const todoList = document.querySelector("ul#todo-list");
+const addTodoInputField = document.querySelector("input");
+const addTodoButton = document.querySelector("button");
+const removeTodoButton = '<button class="remove" type="button">Remove</button>';
 
-// List out each to do items in teh tasks array.
+/*************************************
+ INITIATION
+ 1. Preload the to do items
+ ************************************/
 for (let i = 0; i < tasks.length; i++) {
-  let htmlContent =
-    tasks[i] + "    " + '<button class="remove" type="button">Remove</button>';
-  todoItem = document.createElement("li");
-  todoItem.innerHTML = htmlContent;
-  console.log(todoItem.innerHTML);
-  todoList.appendChild(todoItem);
+  let htmlContent = tasks[i] + removeTodoButton;
+  AddTodoItem(htmlContent);
+  addTodoInputField.value = inputPrompt;
 }
 
-// Click a "done" todo item to add strike-through.
-// Click again to remove strike-through.
+/*************************************
+ TODO ITEM FUNCTIONALITIES
+ 1. User can click on todo time to add strike-through, indicating it's done.
+ 2. Clicks the done todo item to remove strike-through, making it a todo again.
+ 3. User can remove the todo item from the screen by click "Remove" button.
+ ************************************/
+
 todoList.addEventListener("click", function(event) {
-  event.target.classList.toggle("done");
+  event.target.tagName === "BUTTON"
+    ? todoList.removeChild(event.target.parentNode)
+    : event.target.classList.toggle("done");
 });
 
-// Insert a new to do item in todo list by clicking "Add Todo" button.
-let addTodoButton = document.querySelector("button");
-addTodoButton.addEventListener("click", function() {
-  AddTodoItem();
-});
+/*************************************
+ INPUT FIELD FUNCTIONALITIES
+ 1. Input field default value is What's next? to prompt user to input todo.
+ 2. When user click the input field, clear "what's next?" for user input.
+ 3. If user doesn't input anything and clicks other things, show "What's next?" again.
+ 4. If user input some text in the input field and clicks other things, retain user input.
+ 5. When user hits enter and there is text in input field, add user input todo list.
+ 6. User can also use the "Add To Do" button to add todo item to todo list.
+ ************************************/
 
-// Insert a new to do item in todo list by pressing enter in input box.
-let addTodoInputField = document.querySelector("input");
-addTodoInputField.addEventListener("keypress", function(event) {
-  if (event.key === "Enter") {
-    AddTodoItem();
+addTodoInputField.addEventListener("focus", function(event) {
+  if (event.target.value === inputPrompt) {
+    event.target.value = "";
   }
 });
 
+addTodoInputField.addEventListener("blur", function(event) {
+  if (event.target.value === "") {
+    event.target.value = inputPrompt;
+  }
+});
+
+addTodoInputField.addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    let htmlContent = addTodoInputField.value + removeTodoButton;
+    AddTodoItem(htmlContent);
+    addTodoInputField.value = inputPrompt;
+  }
+});
+
+// Insert a new to do item in todo list by clicking "Add Todo" button.
+addTodoButton.addEventListener("click", function() {
+  let htmlContent = addTodoInputField.value + removeTodoButton;
+  AddTodoItem(htmlContent);
+  addTodoInputField.value = inputPrompt;
+});
+
 // Function for adding new todo item to todo list.
-function AddTodoItem() {
-  let htmlContent =
-    document.querySelector("input").value +
-    "    " +
-    '<button class="remove" type="button">Remove</button>';
-  todoItem = document.createElement("li");
-  // todoItem.innerHTML = document.querySelector("input").value;
+function AddTodoItem(htmlContent) {
+  // if (addTodoInputField.value && addTodoInputField.value !== inputPrompt) {
+  let todoItem = document.createElement("li");
   todoItem.innerHTML = htmlContent;
   todoList.appendChild(todoItem);
-  return console.log(todoItem.innerHTML);
+  // }
 }
